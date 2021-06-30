2908fd8aced31c15eeb2ab8908913a6c2cfca3b6
Walacefex <walacefex01@gmail.com>
2021-06-30 00:35:15 -0300

    arrumando espaçamento entre a palavra var e os valores

--- src/components/LinkWrapper/styles.ts
+++ src/components/LinkWrapper/styles.ts
@@ -3,8 +3,8 @@ import styled from 'styled-components'
 export const Wrapper = styled.div`
   position: fixed;
   z-index: 1100;
-  top: var (--medium);
-  right: var (--medium);
+  top: var(--medium);
+  right: var(--medium);
   color: var(--white);
   cursor: pointer;
 
