e5dd01c4565af92639fc261084b9c102a64975f9
Walacefex <walacefex01@gmail.com>
2021-06-30 00:25:12 -0300

    Implementando places no componente Map na index, para mostrar o marcador no mapa

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,5 +3,19 @@ import dynamic from 'next/dynamic'
 const Map = dynamic(() => import('components/Map'), { ssr: false })
 
 export default function Home() {
-  return <Map />
+  return (
+    <Map
+      places={[
+        {
+          id: '1',
+          name: 'São Paulo',
+          slug: 'saopaulo',
+          location: {
+            latitude: -23.5489,
+            longitude: -46.6388
+          }
+        }
+      ]}
+    />
+  )
 }
